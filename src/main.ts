8bf8d49112a9b9fc0fcc82fb7f76000692a4c4f5
Ibis <ibis@reformtek.com.tw>
2024-07-17 09:51:57 +0800

    add vuetify config

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,32 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Icons
+// https://stackoverflow.com/questions/57053728/vuetify-icon-not-showing
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
