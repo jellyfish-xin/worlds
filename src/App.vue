190130abd2ba294d6e13f7702818a8a70b8abcd7
Ibis <ibis@reformtek.com.tw>
2024-08-28 18:08:35 +0800

    update home page

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,7 @@
     <v-main>
       <RouterView />
     </v-main>
-
-    <v-speed-dial class="world-view-list" location="top center" transition="scale-transition">
-      <template v-slot:activator="{ props: activatorProps }">
-        <v-fab v-bind="activatorProps" size="large" icon="$vuetify"></v-fab>
-      </template>
-
-      <v-btn
-        v-for="(item, index) in worldViewItems"
-        :key="index"
-        :icon="item.icon"
-        @click="router.push({ name: item.value })"
-      >
-      </v-btn>
-    </v-speed-dial>
   </v-app>
 </template>
 
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { routeName } from '@/router'
-
-const router = useRouter()
-
-const worldViewItems = [
-  { label: '旅歌', icon: 'mdi-numeric-3-box-outline', value: routeName.travelSong },
-  { label: 'méduse', icon: 'mdi-numeric-2-box-outline', value: routeName.meduse },
-  { label: 'ω', icon: 'mdi-numeric-1-box-outline', value: routeName.omega },
-  { label: 'Home', icon: 'mdi-numeric-1-box-outline', value: routeName.home }
-]
-</script>
+<script setup lang="ts"></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,52 @@
 <template>
   <v-container fluid class="home-page">
     <v-row>
-      <v-col cols="12" md="8">
-        <h1 class="about-title">About</h1>
-
-        <v-icon>mdi-email-outline</v-icon>
-        <p>kurage.xin@gmail.com</p>
-      </v-col>
-      <v-col cols="12" md="4">
+      <v-col cols="12" md="8" class="about">
         <v-row>
-          <v-col
-            cols="12"
-            md="12"
-            sm="6"
-            class="social-platform"
-            v-for="(item, index) in socialPlatformItems"
-            :key="index"
-          >
+          <v-col cols="12" sm="5">
+            <h1 class="about-title">Worlds</h1>
+          </v-col>
+          <v-col cols="12" sm="7" class="tag">
+            <span v-for="(item, index) in tagItems" :key="index" class="tag-list">{{ item }}</span>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <p>歡迎來到所有世界的入口⋯⋯</p>
+
+            <p>
+              望可以的時候就有臉書，晚上關係開心的被的名的⋯去看看我做。單的巴是我比較就。先去以有，在手先生的都覺得，真的是時，只是煩做得但還是獎音樂西都，就當日向翔，在沒用欸不是東有沒有啊啊啊。等等會在說過我。單的巴是我比較就。先去以有，在手先生的都覺得，真的是時，只是煩做得但還是獎音樂西都，就當日向翔，在沒用欸不是東有沒有啊啊啊。等等會在說過我。
+            </p>
+
+            <p>
+              角色的是的願望，魔法師所以像是在。實答明期的一個滅睡事情，晚黑對方：麼很一堆的遊。怎麼根本夢哈哈哈，泡麵知道我也不日以這，或是喜歡的代誕禮物回家，明年離開前道出，就的事現還是納哈特來才。
+            </p>
+
+            <div class="social-platform">
+              <button v-for="(item, index) in socialPlatformItems" :key="index">
+                <v-avatar
+                  class="social-platform-list"
+                  :image="item.logo"
+                  @click="openInNew(item.url)"
+                />
+              </button>
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="12" md="4" class="world">
+        <v-row>
+          <v-col cols="12" md="12" sm="6" v-for="(item, index) in worldItems" :key="index">
             <v-card
-              class="social-platform-list"
+              class="world-list"
               :prepend-avatar="item.prependAvatar"
               :title="item.title"
               :subtitle="item.subtitle"
             >
               <v-card-actions>
-                <v-btn icon="mdi-open-in-new" @click="follow(item.url)" />
+                <v-btn icon="mdi-open-in-new" @click="openInNew(item.routeName)" />
               </v-card-actions>
             </v-card>
           </v-col>
@@ -39,22 +61,47 @@
 </template>
 
 <script setup lang="ts">
-const socialPlatformItems = [
+import { routeName } from '@/router';
+
+const tagItems = ['原創', '設定', '世界觀']
+
+const worldItems = [
   {
-    prependAvatar: '/plurk-icon.png',
-    title: 'Plurk',
+    prependAvatar: '/',
+    title: 'ω',
     subtitle: '@jellyfish_xin',
-    url: 'https://www.plurk.com/jellyfish_xin'
+    routeName: routeName.omega
   },
   {
-    prependAvatar: '/clibo-icon.png',
-    title: 'Clibo',
+    prependAvatar: '/',
+    title: 'méduse',
     subtitle: '@xin',
-    url: 'https://clibo.tw/users/xin'
+    routeName: routeName.meduse
+  },
+  {
+    prependAvatar: '/',
+    title: '旅歌',
+    subtitle: '@xin',
+    routeName: routeName.travelSong
   }
 ]
 
-function follow(url: string) {
+const socialPlatformItems = [
+  {
+    logo: '/plurk-logo.png',
+    url: 'https://www.plurk.com/jellyfish_xin'
+  },
+  {
+    logo: '/clibo-logo.png',
+    url: 'https://clibo.tw/users/xin'
+  },
+  {
+    logo: '/github-logo.png',
+    url: 'https://github.com/jellyfish-xin'
+  }
+]
+
+function openInNew(url: string) {
   window.open(url, '_blank')
 }
 </script>
